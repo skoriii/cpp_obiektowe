a0cf25243b0697a358f1247869ef575fc5b1a049
skoriii <jula.skor@wp.pl>
2024-11-29 03:17:23 +0100

    dodano kod z zadaniem 1

---
+++ ex1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Sequence {
+public:
+    Sequence() : header(""), sequence("") {}
+
+    // wczytania pliku fasta
+    void readFasta(const string& filepath) {
+        ifstream file(filepath);
+
+        string line;
+        bool isHeader = true;
+
+        while (getline(file, line)) {
+            if (line.empty()) continue; // Pomijamy puste linie
+
+            if (line[0] == '>') { // Nagłówek
+                header = line.substr(1); // Usuwamy znak '>'
+                isHeader = false;
+            }
+            else if (!isHeader) { // Sekwencja
+                sequence += line;
+            }
+        }
+
+        file.close();
+
+        // output wczytanych plikow
+        cout << "Naglowek: " << header << endl;
+        cout << "Sekwencja: " << sequence << endl;
+    }
+
+private:
+    string header;   // Nagłówek FASTA
+    string sequence; // Sekwencja
+};
+
+int main() {
+    Sequence seq;
+
+    seq.readFasta("ex1/DNA.FASTA");
+
+    return 0;
+}
